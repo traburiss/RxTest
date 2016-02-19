d1e7fcaa6f9ae47dc68a642c286fbcf4a926b02c
traburiss <marlycool@qq.com>
2016-02-19 10:06:16 +0800

    Initial commit

---
+++ app/src/main/java/com/litc/rxtest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.litc.rxtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import lomboktest.LombokTest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button) Button button;
+    @Bind(R.id.textView) TextView textView1;
+    @Bind(R.id.textView2) TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        textView1.setText(R.string.butter_knife_test);
+        button.setOnClickListener((view)-> Toast.makeText(this, R.string.lambda_test, Toast.LENGTH_SHORT).show());
+
+        LombokTest lombokTest = new LombokTest(10,0.1);
+        textView2.setText(lombokTest.toString());
+    }
+}
---
+++ app/src/main/java/lomboktest/LombokTest.java
@@ -0,0 +1,19 @@
+package lomboktest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Created by Traburiss on 2016/2/19.
+ * Version 1.0
+ * Description
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LombokTest {
+    private int i;
+    private double d;
+}
