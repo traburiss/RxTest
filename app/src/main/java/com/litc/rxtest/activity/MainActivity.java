4a18e6ca3985cc6efa51fcb736a49182119f0b8b
traburiss <marlycool@qq.com>
2016-06-29 14:23:55 +0800

    nothing

--- app/src/main/java/com/litc/rxtest/activity/MainActivity.java
+++ app/src/main/java/com/litc/rxtest/activity/MainActivity.java
@@ -1,23 +1,35 @@
 package com.litc.rxtest.activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.litc.rxtest.R;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-
-import com.litc.rxtest.lombok.LombokTest;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.button) Button button;
     @Bind(R.id.textView) TextView textView1;
     @Bind(R.id.textView2) TextView textView2;
+
+    @Bind(R.id.button1) Button button1;
+    @Bind(R.id.button2) Button button2;
+    @Bind(R.id.button) Button button;
+    @Bind(R.id.button5) Button button5;
+
+    @Bind(R.id.editText) EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +37,158 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+        initDate();
+        initView();
+        initEven();
+    }
+
+    private void initDate() {
+
+
+    }
+
+    private void initView() {
+
         textView1.setText(R.string.butter_knife_test);
 
-        button.setOnClickListener((view) -> {
-//            Toast.makeText(this, R.string.lambda_test, Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
+    }
+
+    private void initEven() {
+
+        RxView.clicks(button1).subscribe(this::Button2Click);
+
+        button2.setOnClickListener(view -> {
+
+            query(" litc")
+                    .flatMap(Observable::from)
+                    .flatMap(this::getTitle)
+                    .filter(s -> !s.contains("hello"))
+                    .take(100)
+                    .doOnNext(s -> textView2.setText(s))
+                    .subscribe(s -> Toast.makeText(this, s, Toast.LENGTH_SHORT).show());
         });
 
-        LombokTest lombokTest = new LombokTest(10,0.1);
-        textView2.setText(lombokTest.toString());
+        button.setOnClickListener(view -> {
+
+            queryNums()
+                    .flatMap(Observable::from)
+                    .flatMap(this::Turn2Num)
+                    .flatMap(this::add2String)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new mySubscriber<String>());
+        });
+
+        RxView.clicks(button5)
+                .subscribe(this::button5Click);
+    }
+
+    private void Button2Click(Void v){
+
+        Observable.just(1234567890)
+                .map(s -> s + " litc")
+                .subscribe(s -> Toast.makeText(this, s, Toast.LENGTH_SHORT).show());
+
+    }
+
+    private Observable<ArrayList<String>> query(String s){
+
+        ArrayList<String> list = new ArrayList<String>();
+        Observable.from(new String[]{s + " hello world", s + " is good", s + " 你好世界", s + " 爽歪歪", s + "世界末日", s + "hello e", s + "abc"}).subscribe(list::add);
+        return Observable.just(list);
+    }
+
+    private Observable<String> getTitle(String s){
+
+        s += " title";
+        return Observable.just(s);
+    }
+
+    private Observable<ArrayList<String>> queryNums(){
+
+        ArrayList<String> list = new ArrayList<String>();
+        Observable.from(new String[]{"1", "3", "5", "8", "10"}).subscribe(list::add);
+        return Observable.just(list);
+    }
+
+    private Observable<ArrayList<String>> queryNums(Void v){
+
+        ArrayList<String> list = new ArrayList<String>();
+        Observable.from(new String[]{"1", "3", "5", "8", "10"}).subscribe(list::add);
+        return Observable.just(list);
+    }
+
+    private Observable<Integer> Turn2Num(String s){
+
+        return Observable.just(Integer.valueOf(s));
+    }
+
+    private Observable<String> add2String(int num){
+
+        int m = 0;
+        for(long i = 0; i < 100000000; i++){
+
+            m += i%2;
+        }
+        return Observable.just(String.valueOf(num*m));
+    }
+
+    private String dealWithString(String s){
+
+
+        int m = 0;
+        for(long i = 0; i < 100000000; i++){
+
+            m += i%2;
+        }
+        return s + m;
+    }
+
+    private Observable<String> newDealWithString(String s){
+
+        return Observable
+                .defer(()->Observable.just(dealWithString(s)));
+    }
+
+    private void button5Click(Void v){
+
+
+        newDealWithString(editText.getText().toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new mySubscriber<String>());
+    }
+
+    private class mySubscriber<T> extends Subscriber<T>{
+
+        @Override
+        public void onCompleted() {
+
+            Toast.makeText(MainActivity.this, "finish", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNext(T t) {
+
+            dealWithT(t);
+        }
+
+        private void dealWithT(T t){
+
+            if (t.getClass().isAssignableFrom(String.class)){
+
+                Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+            }else{
+
+                Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
